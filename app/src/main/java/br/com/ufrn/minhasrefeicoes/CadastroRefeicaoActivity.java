f8bc35c86e392dfaefabbbb06b86b7846008df15
Ubirajara Júnior <ubirajara.viegas@yahoo.com.br>
2022-01-10 15:22:24 -0300

    Ajustes em itens da UI de activity_cadastro_refeicao. Adiçao de código responsável pela inicializaçao do spinner dos tipos de refeiçao

--- app/src/main/java/br/com/ufrn/minhasrefeicoes/CadastroRefeicaoActivity.java
+++ app/src/main/java/br/com/ufrn/minhasrefeicoes/CadastroRefeicaoActivity.java
@@ -3,12 +3,21 @@ package br.com.ufrn.minhasrefeicoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CadastroRefeicaoActivity extends AppCompatActivity {
+
+    private Spinner spTiposRefeicao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_refeicao);
+
+        spTiposRefeicao = (Spinner) findViewById(R.id.spTiposRefeicao);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tipos_refeicao, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTiposRefeicao.setAdapter(adapter);
     }
 }
